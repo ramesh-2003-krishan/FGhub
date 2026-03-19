61b249d32fff60aeadd156b4f8a26d1f5a202d30
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-03-19 23:32:37 +0530

    add user for logging page

---
+++ backend/FGhub-backend/src/main/java/com/FGhub/FGhub_backend/model/user.java
@@ -0,0 +1,29 @@
+package com.FGhub.FGhub_backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class user {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+
+    public user() {}
+
+    public user(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
