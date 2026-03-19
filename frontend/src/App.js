8e534f584a20ac290ba0598f99e612e4d1022cf5
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-03-20 00:28:50 +0530

    make changes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
-      <Home />
+      {isLoggedIn ? (
+        <Home />
+      ) : (
+        <Login setIsLoggedIn={setIsLoggedIn} />
+      )}
     </div>
   );
 }
